eaad41ef38b7ef1610c8fb79c4630bf65830bf4b
Neyza17 <nesyaratuanastasya@gmail.com>
2025-01-19 00:02:05 +0700

    second commit

--- PemilihanBilangan17.java
+++ PemilihanBilangan17.java
@@ -7,10 +7,10 @@ public class PemilihanBilangan17 {
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
 
-        if (angka % 2 == 0) {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        } else {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        String hasil = (angka % 2 == 0) ? "genap" : "ganjil";
+
+        System.out.println("Angka " + angka + " termasuk bilangan " + hasil);
+
+        sc.close();
     }
 }
